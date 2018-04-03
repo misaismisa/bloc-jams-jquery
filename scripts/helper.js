925ae3654445ece27ef160b7c0d2f56086de14d8
Misa Nguyen <misanguyen1390@gmail.com>
2018-04-03 12:46:00 -0700

    total time displayed in min secs

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,23 +1,8 @@
 class Helper {
-  playPauseAndUpdate (song) {
-    helper.playPauseAndUpdate();
-
-    $('#time-control input').on('input', function (event) {
-      player.skipTo(event.target.value);
-    });
-
-    setInterval( () => {
-      if (player.playState !== 'playing') {return;}
-      const currentTime = player.getTime();
-      const duration = player.getDuration();
-      const percent = (currentTime / duration) * 100;
-      $('#time-control .current-time').text( currentTime );
-      $('#time-control .total-time').text( duration );
-      $('#time-control input').val(percent);
-    }, 1000);
-
-  };
-
+  playPauseAndUpdate(song) {
+    player.playPause(newSong);
+    const duration = song.duration;
+    $('#time-control .total-time').text( player.prettyTime(duration) );
+  }
 }
-
-const helper = new Helper();
+const helper = new Helper
